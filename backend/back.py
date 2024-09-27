5dbc21dd94fee18a62253b58b00d2db7ed41486d
ph delville <phdelville33@gmail.com>
2024-09-27 18:43:17 +0200

    fast_api working locally + added predict function

--- backend/back.py
+++ backend/back.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from backend.api.geocode import get_coordinates
-from backend.api.open_meteo import get_temperature
+from backend.api.open_meteo import get_temperature, get_history
+from backend.model.LSTM_functions import fit_LSTM_model, predict_from_model, load_LSTM_model
+import datetime
+import pandas as pd
+import numpy as np
+import os
 app = FastAPI()
 
 # define a root '/' endpoint
 @app.get("/")
 def index():
-    return {"message": "Hello, World"}
+    return {"message": "It works!"}
 
 @app.get("/hello/{name}")
 def hello_name(name: str):
@@ -14,7 +19,50 @@ def hello_name(name: str):
 
 @app.get("/temperature/{city}")
 def temperature(city: str):
+    """
+    Retrieves the temperature for a given city.
+    This function takes the name of a city, fetches its geographical coordinates,
+    and then retrieves the current temperature for those coordinates. The temperature
+    is added to the data dictionary and returned.
+    Args:
+        city (str): The name of the city for which to retrieve the temperature.
+    Returns:
+        dict: A dictionary containing the city's coordinates and temperature.
+    """
     data = get_coordinates(city)
     temp = get_temperature(data['lat'], data['lng'])
     data['temperature'] = temp
     return data
+
+@app.get("/predict_temperature/{city}")
+def prediction(city: str):
+    """
+    Predicts the temperature for a given city using historical weather data and a pre-trained LSTM model.
+    Args:
+        city (str): The name of the city for which the temperature prediction is to be made.
+    Returns:
+        List[Dict[str, Any]]: A list of dictionaries containing the predicted temperatures for the next 24 hours.
+            Each dictionary has the following keys:
+                - 'date': The date and time of the prediction.
+                - 'predicted_temperature': The predicted temperature rounded to one decimal place.
+    """
+
+    # get the coordinates of the city
+    spatial_data = get_coordinates(city)
+    # get the date of yesterday and previous week
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=2)
+    yesterday = yesterday.strftime("%Y-%m-%d")
+    previous_week = datetime.datetime.now() - datetime.timedelta(days=16)
+    previous_week = previous_week.strftime("%Y-%m-%d")
+    # get previous week data from the Open-Meteo API for the city
+    history = get_history(spatial_data['lat'], spatial_data['lng'], previous_week, yesterday)
+    df = pd.DataFrame({'date': history['time'], 'temperature': history['temperature_2m']})
+    df['date'] = pd.to_datetime(df['date'])
+    # Load model and scaler
+    scaler_path = os.path.join(os.path.dirname(__file__), 'model','model_saved', 'saved_models_scaler.pkl')
+    model_path = os.path.join(os.path.dirname(__file__), 'model','model_saved', 'saved_models.keras')
+    model , scaler = load_LSTM_model(model_path,scaler_path)
+    forecast = predict_from_model(df, model, scaler)
+    forecast = forecast[-24:]
+    forecast['predicted_temperature'] = forecast['predicted_temperature'].apply(lambda x: round(x, 1))
+    return forecast.to_dict(orient="records")
--- backend/model/LSTM_functions.py
+++ backend/model/LSTM_functions.py
@@ -1,59 +1,41 @@
-from backend.api.geocode import get_coordinates
-from backend.api.open_meteo import get_temperature, get_history
-import requests
 import dotenv
 import numpy as np
-import os
 import pandas as pd
-import matplotlib.pyplot as plt
+import os
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-from pmdarima import auto_arima
-from datetime import datetime, timedelta
-from pandas.plotting import autocorrelation_plot
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-from pmdarima import auto_arima
-
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+from keras.callbacks import EarlyStopping
+import joblib
 dotenv.load_dotenv()
 
 
-def initialize_LSTM_model(sequence_length, forecast_horizon):
+def initialize_LSTM_model(sequence_length=336, forecast_horizon=48):
     """
     Initializes and returns an LSTM model for time series forecasting.
     The model consists of two LSTM layers with 100 units each, followed by dropout layers to prevent overfitting,
-    and a dense layer with a single unit for the output. The model is compiled with the Adam optimizer and
+    and a dense layer with 'forecast_horizon' units for the output. The model is compiled with the Adam optimizer and
     mean squared error loss function, and it also tracks mean absolute error as a metric.
+
     Parameters:
     sequence_length (int): The length of the input sequences.
+    forecast_horizon (int): The number of future time steps to forecast.
+
     Returns:
     keras.models.Sequential: The compiled LSTM model.
     """
-
     model = Sequential()
     model.add(LSTM(100, return_sequences=True, input_shape=(sequence_length, 1)))
     model.add(Dropout(0.2))
-    model.add(LSTM(100, return_sequences=False))
+    model.add(LSTM(100, return_sequences=False))  # `return_sequences=False` for the final LSTM layer
     model.add(Dropout(0.2))
-    model.add(Dense(forecast_horizon))
+    model.add(Dense(forecast_horizon))  # Output layer for 'forecast_horizon' predictions
     model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+
     return model
 
-def data_daily(history):
-    """
-    Converts hourly weather data to daily averages.
-    Parameters:
-    history (dict): A dictionary containing 'time' and 'temperature_2m' keys with corresponding lists of hourly data.
-    Returns:
-    pd.DataFrame: A DataFrame with daily averaged temperature data, indexed by date.
-    """
-    df = pd.DataFrame({'date': history['time'], 'temperature': history['temperature_2m']})
-    df['date'] = pd.to_datetime(df['date'])
-    df.set_index('date', inplace=True)
-    df_daily = df.resample('D').mean()
-    df_daily.dropna(inplace=True)
-    return df_daily
+
 def scale_data(df):
     """
     Scales the temperature data in the given DataFrame using MinMaxScaler.
@@ -70,7 +52,7 @@ def scale_data(df):
     scaled_temps = scaler.fit_transform(temperature)
     return scaled_temps, scaler
 
-def create_sequences(data, sequence_length, forecast_horizon=1):
+def create_sequences(data, sequence_length=336, forecast_horizon=48):
     """
     Generates sequences of data for time series forecasting.
     Parameters:
@@ -90,26 +72,84 @@ def create_sequences(data, sequence_length, forecast_horizon=1):
     return np.array(x), np.array(y)
 
 
-def fit_LSTM_model(data, split_factor = 0.8, sequence_length = 168, forecast_horizon = 24):
+def fit_LSTM_model(data, split_factor=0.8, sequence_length=336, forecast_horizon=48, model_name=None):
     """
     Fits an LSTM model to the provided time series data.
+
     Parameters:
     data (pd.DataFrame or np.ndarray): The input time series data.
     split_factor (float, optional): The fraction of data to be used for training. Default is 0.8.
-    sequence_length (int, optional): The length of the input sequences. Default is 168.
-    forecast_horizon (int, optional): The number of time steps to forecast. Default is 24.
+    sequence_length (int, optional): The length of the input sequences. Default is 336 for 2 weeks.
+    forecast_horizon (int, optional): The number of time steps to forecast. Default is 48 for 2 days.
+
     Returns:
     tuple: A tuple containing:
         - history (keras.callbacks.History): The history object generated after training the model.
         - model (keras.Model): The trained LSTM model.
         - scaler (sklearn.preprocessing.StandardScaler): The scaler used to normalize the data.
     """
-
+    # Scale the data
     scaled_temps, scaler = scale_data(data)
-    X, y = create_sequences(scaled_temps, sequence_length,forecast_horizon)
-    split = int(split_factor * len(X))  # 80% pour l'entraînement, 20% pour le test
+
+    # Create sequences for training
+    X, y = create_sequences(scaled_temps, sequence_length, forecast_horizon)
+
+    # Split into training and testing sets
+    split = int(split_factor * len(X))  # 80% training, 20% testing
     X_train, X_test = X[:split], X[split:]
     y_train, y_test = y[:split], y[split:]
+
+    # Initialize the LSTM model
     model = initialize_LSTM_model(sequence_length, forecast_horizon)
-    history = model.fit(X_train, y_train, epochs=5, batch_size=64, validation_data=(X_test, y_test))
+
+    # Train the model
+    history = model.fit(X_train, y_train, epochs=30, batch_size=64, validation_data=(X_test, y_test),
+                        callbacks=[EarlyStopping(patience=2)], verbose=1)
+    # Save the model
+    if model_name:
+        save_dir = os.path.join(os.path.dirname(__file__), 'model_saved')
+        model.save(os.path.join(save_dir, f'{model_name}.keras'))
+
+    # Save the scaler using joblib
+    if model_name:
+        save_dir = os.path.join(os.path.dirname(__file__), 'model_saved')
+        joblib.dump(scaler, os.path.join(save_dir, f'{model_name}_scaler.pkl'))
     return history, model, scaler
+
+def load_LSTM_model(model_path, scaler_path):
+    """
+    Load a pre-trained LSTM model and its corresponding scaler from disk.
+    Parameters:
+    model_path (str): The file path to the saved model.
+    scaler_path (str): The file path to the saved scaler.
+    Returns:
+    tuple: A tuple containing:
+        - model (keras.Model): The loaded LSTM model.
+        - scaler (sklearn.preprocessing.StandardScaler): The loaded scaler object.
+    """
+    model = tf.keras.models.load_model(model_path)
+    scaler = joblib.load(scaler_path)
+    return model, scaler
+
+def predict_from_model(df, model, scaler, sequence_length=336, forecast_horizon=48):
+    """
+    Predict future temperatures using a trained LSTM model.
+    Parameters:
+    data (pd.DataFrame): The input data containing a 'temperature' column and a datetime index.
+    model (keras.Model): The trained LSTM model used for prediction.
+    scaler (sklearn.preprocessing.StandardScaler): The scaler used to normalize and inverse transform the data.
+    sequence_length (int, optional): The length of the input sequences for the LSTM model. Default is 168.
+    forecast_horizon (int, optional): The number of future time steps to predict. Default is 24.
+    Returns:
+    pd.DataFrame: A DataFrame containing the predicted temperatures with corresponding dates.
+    """
+    data = df[-sequence_length:]
+    temperature_to_pred = data['temperature'].values.reshape(-1, 1)
+    scaled_temps_to_pred = scaler.transform(temperature_to_pred)
+    X = scaled_temps_to_pred[-sequence_length:].reshape(1,sequence_length, 1)
+    forecast_scaled = model.predict(X)
+    forecast = scaler.inverse_transform(forecast_scaled)
+    next_day_dates = pd.date_range(data['date'].iloc[-1] + pd.Timedelta(hours=1), periods=forecast_horizon, freq='H')
+    df_forecast = pd.DataFrame({'date': next_day_dates, 'predicted_temperature': forecast[0]})
+
+    return df_forecast
