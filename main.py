d7207a5c428c0f3a0ff55fb83a2fe38f4a904363
ph delville <phdelville33@gmail.com>
2024-10-02 17:36:00 +0200

    changing the url of the api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from frontend.streamlit_app import run_app
 
 if __name__ == "__main__":
-    run_app(url='http://127.0.0.1:8000')  # Lancer l'application Streamlit
+    run_app(url='https://weather-service-3dmc3uyxya-ew.a.run.app')  # Lancer l'application Streamlit
