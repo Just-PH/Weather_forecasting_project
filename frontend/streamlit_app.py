9d3abb0f48e8adf35efb6426bc0511bda30594d9
ph delville <phdelville33@gmail.com>
2024-10-02 10:34:45 +0200

    Requirements and streamlit update with plotly

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -4,7 +4,8 @@ import os
 import requests
 from datetime import datetime
 import pandas as pd  # Assurez-vous d'importer pandas
-
+import plotly.express as px
+import plotly.graph_objects as go
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image:
         encoded_string = base64.b64encode(image.read())
@@ -63,10 +64,10 @@ def run_app(url):
         st.session_state.prediction_result = None
 
     # Ajouter les boutons sur la même ligne avec `st.columns`
-    col1, col2 = st.columns(2)
+    col1, col2 = st.columns([1,2])
 
     with col1:
-        if st.button("Température du jour"):
+        if st.button("Température actuelle"):
             response = requests.get(f"{url}/temperature/{city_name}")
             if response.status_code == 200:
                 data = response.json()
@@ -74,7 +75,9 @@ def run_app(url):
                 lng = data.get('lng')
                 temperature = data.get('temperature')
                 if lat and lng:
-                    st.session_state.temperature_result = f"La température actuelle à {city_name} est de {temperature}°C"
+                    st.session_state.temperature_result = f"<span style='font-size:24px; font-weight:bold;'>\
+                        La température actuelle à {city_name} est de {temperature}°C</span>"
+
                 else:
                     st.session_state.temperature_result = "Ville introuvable, veuillez essayer à nouveau."
             else:
@@ -93,14 +96,30 @@ def run_app(url):
     # Afficher les résultats sous les boutons
     with col1:
         if st.session_state.temperature_result:
-            st.write(st.session_state.temperature_result)
+            st.markdown(st.session_state.temperature_result, unsafe_allow_html=True)
 
     with col2:
-        if st.session_state.prediction_result:
-            if isinstance(st.session_state.prediction_result, dict):
-                # Convertir le dictionnaire en DataFrame
-                df = pd.DataFrame(list(st.session_state.prediction_result.items()), columns=['Heure', 'Température (°C)']).set_index('Heure')
-                st.write(f"Prédictions pour {city_name} :")
-                st.dataframe(df)  # Afficher le tableau
-            else:
-                st.error(st.session_state.prediction_result)
+        if isinstance(st.session_state.prediction_result, dict):
+            # Convertir le dictionnaire en DataFrame
+            df = pd.DataFrame(list(st.session_state.prediction_result.items()), columns=['Heure', 'Température']).set_index('Heure')
+            x_values = df.index
+            y_values = df['Température']
+            fig = go.Figure()
+
+            fig.add_trace(go.Scatter(
+                x=x_values,
+                y=y_values,
+                mode='lines+markers',
+                marker=dict(size=10, color='lightblue'),
+                line_shape='spline',
+                hovertemplate='<span style="font-size: 18px;">%{x} : T = %{y}°C</span>',
+                name=""
+            ))
+            fig.update_layout(
+                title=f"Prédiction des températures pour {city_name}",
+                xaxis_title="Heure",
+                yaxis_title="Température (°C)",
+                xaxis_tickangle=-45,
+                showlegend=False,
+            )
+            st.plotly_chart(fig, use_container_width=True)
