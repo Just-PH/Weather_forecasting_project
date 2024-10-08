7683652fb037c729ff28aff5c886e32ce579b61d
ph delville <phdelville33@gmail.com>
2024-10-08 13:04:41 +0200

    API call to the back work (fixed the cors problem)

--- backend/back.py
+++ backend/back.py
@@ -6,7 +6,18 @@ import datetime
 import pandas as pd
 import numpy as np
 import os
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # define a root '/' endpoint
 @app.get("/")
--- weather-forecast-frontend/src/App.js
+++ weather-forecast-frontend/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [cityName, setCityName] = useState('Paris');
+  const [temperature, setTemperature] = useState(null);
+  const API_URL = process.env.REACT_APP_BACK_API; // Assure-toi que cela est bien défini dans .env
+
+  const handleTemperatureFetch = async () => {
+    try {
+      // Requête API directe à l'URL complète
+      const response = await axios.get(`${API_URL}/temperature/${cityName}`);
+      if (response.status === 200) {
+        const data = response.data;
+        setTemperature(data.temperature); // Assure-toi que la structure de la réponse est correcte
+      } else {
+        setTemperature(`Erreur lors de la requête API.`);
+      }
+    } catch (error) {
+      setTemperature(`Erreur lors de la requête API. (erreur : ${error.message})`);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Météo du Jour</h1>
+      <input
+        type="text"
+        value={cityName}
+        onChange={(e) => setCityName(e.target.value)}
+        placeholder="Entrez le nom de la ville"
+      />
+      <button onClick={handleTemperatureFetch}>Température actuelle</button>
+      {temperature && (
+        <div>
+          <h2>La température actuelle à {cityName} est de {temperature}°C</h2>
+        </div>
+      )}
     </div>
   );
 }
